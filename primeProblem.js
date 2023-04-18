c3cd054fded6ea260c81bcc8d9b5c258635daa93
Mohd Asif <asif@asif.local>
2023-04-18 23:22:18 +0530

    primeProblem.js code corrected

--- primeProblem.js
+++ primeProblem.js
@@ -1,11 +1,13 @@
 let n=23;
+let count=0;
 for(let i=1;i<=n;i++){
     if(n%i==0){
         count++;
     }
+    
 }
-if(count==1){
+if(count==2){
     console.log('prime number')
 }else{
-    console.log('prime number')
+    console.log('Not a prime number')
 }
